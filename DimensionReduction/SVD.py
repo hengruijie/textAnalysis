561c916d68d57fdc0a21d099af63527352ad98fd
hengruijie <heng_rui_jie@nyp.edu.sg>
2022-12-01 09:01:28 +0800

    Adding GitIgnore and Comments for SVD

---
+++ DimensionReduction/SVD.py
@@ -0,0 +1,11 @@
+"""
+Title: Singular Value Decomposition
+Description: Used in applications to perform Dimension Reductons, image compression
+Intuition: Approximating a high dimension dataset to a low-rank dataset 
+
+Software Version:
+- Python 3.8.2
+
+"""
+
+
